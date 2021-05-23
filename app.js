280af518551620fc1f9911fe5d595359ba1db291
Abhay Tomar <abhaytomar155@gmail.com.com>
2021-05-23 08:24:47 +0530

    5- Added Cookies and sessions for authentication with passportjs

--- app.js
+++ app.js
@@ -1,94 +1,149 @@
-    //jshint esversion:6
-    require('dotenv').config()
-    
+        //jshint esversion:6
+        require('dotenv').config()
+        
 
-    const express = require('express');
-    const bodyParser = require('body-parser');
-    const ejs = require('ejs');
-    const mongoose = require('mongoose');
+        const express = require('express');
+        const bodyParser = require('body-parser');
+        const ejs = require('ejs');
+        const mongoose = require('mongoose');
+        const session = require('express-session');
+        const passport = require('passport');
 
-    //-- using salting and hashing for more protection----------------
+        const passportLocalMongoose = require('passport-local-mongoose');
 
-    const bcrypt =  require('bcrypt');
+        const app = express();
 
-    const saltRounds =  10;
+        app.set('view engine','ejs');
+        app.use(bodyParser.urlencoded({extended:true}));
+        app.use(express.static('piblic'));
+
+        app.use(session({
+            secret:"My important Secret",
+            resave:false,
+            saveUninitialized:false
+        }));
+
+        app.use(passport.initialize());
+        app.use(passport.session());
 
 
-    const app = express();
 
-    app.set('view engine','ejs');
-    app.use(bodyParser.urlencoded({extended:true}));
-    app.use(express.static('piblic'));
+        mongoose.connect('mongodb://localhost:27017/userDB',({useNewUrlParser:true,useUnifiedTopology:true}));
+        mongoose.set('useCreateIndex',true);
 
-    // Level-1 Security is just checking the email and password of the user for this we have to create mongoose database-------------
+        const userSchema = new mongoose.Schema({
+            email: String,
+            password:String
+        });
 
-    mongoose.connect('mongodb://localhost:27017/userDB',({useNewUrlParser:true,useUnifiedTopology:true}));
+        userSchema.plugin(passportLocalMongoose);
 
-    const userSchema = new mongoose.Schema({
-        email: String,
-        password:String
-    });
+        
+        const User = new mongoose.model('User',userSchema);
+        passport.use(User.createStrategy());
 
-    
-    const User = new mongoose.model('User',userSchema);
-    app.route('/')
-    .get((req,res) => {
-        res.render('home');
 
-    });
+      
 
-    app.route('/register')
-    .get((req,res) =>{
-        res.render('register');
-    })
+        passport.serializeUser(function(user, done) {
+            done(null, user);
+          });
+          
+          passport.deserializeUser(function(user, done) {
+            done(null, user);
+          });
 
-    .post((req,res) => {
-        bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
-            // Store hash in your password DB.
+        
 
-            const newUser = new User({
-                email:req.body.username,
-                password:hash
-            });
-    
-            newUser.save(err => {
-                if(!err) {
-                    res.render('secrets')
-                } else {
+
+
+        app.route('/')
+        .get((req,res) => {
+            res.render('home');
+
+        });
+
+
+      
+//----register route----------------------------------------------------
+
+        app.route('/register')
+        .get((req,res) =>{
+            res.render('register');
+        })
+
+        .post((req,res) => {
+            User.register({username: req.body.username},req.body.password,(err,user) => {
+
+                if(err) {
                     console.log(err);
+                    res.redirect('/register')
+                } else {
+
+                    passport.authenticate('local')(req,res,function() {
+
+                        res.redirect('/secrets')
+                    })
+                          
                 }
-        })      
-        }); 
-    });
+            })
+        });
 
-    app.route('/login')
-    .get((req,res) => {
+//------------Secrets route---------------------------------------------
 
 
-        res.render('login');
 
-    })
-
-    .post((req,res) => {
-
-        const username = req.body.username;
-        const password = req.body.password;
-
-        User.findOne({email:username},(err,foundUser) => {
-            if(err) {
-                console.log(err);
+        app.route('/secrets')
+        .get((req,res) => {
+            if (req.isAuthenticated()) {
+                res.render('secrets')
             } else {
-                if (foundUser) {
-                    bcrypt.compare(password, foundUser.password).then(function(result) {
-                        if( result == true) {
-                            res.render('secrets');
-                        }
-                    });             
-                }
+                res.redirect('/login')
             }
         });
-    });
 
-    app.listen(3000,(req,res) => {
-        console.log("Server is running at port 3000");
-    });
+ //----------------Login route---------------------------------
+
+
+        app.route('/login')
+        .get((req,res) => {
+
+
+            res.render('login');
+
+        })
+
+        .post((req,res) => {
+
+            const user = new User({
+                email:req.body.username,
+                password:req.body.password
+            });
+
+            req.login(user,(err) => {
+                if (err) {
+                    console.log(err);
+                    res.redirect('/register')
+                } else {
+                    passport.authenticate('local')(req,res,function() {
+
+                        res.redirect('/secrets')
+                    })
+               
+
+                }
+            })          
+        });
+
+//-----logout route----------------------------------------------
+
+       
+        app.route('/logout')
+        .get((req,res) => {
+            req.logout();
+            res.redirect('/');
+        });
+
+        app.listen(3000,(req,res) => {
+            console.log("Server is running at port 3000");
+        });
